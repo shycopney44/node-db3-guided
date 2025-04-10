62469f06acc2afd449284d9472152f4d16e35762
Shy Copney <shydashacopney@gmail.com>
2025-04-10 15:13:37 -0400

    complete project

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -8,8 +8,27 @@ module.exports = {
   remove
 }
 
-function findPosts(user_id) {
+async function findPosts(user_id) {
+ const rows = await db('posts as p')
+    .select('p.id as post_id', 'contents', 'username')
+    .join('users as u', 'u.id', 'p.user_id')
+    .where('id', user_id)
+
+    console.log(rows)
+    return rows
+ 
+ 
   /*
+select
+    p.id as post_id,
+    contents,
+    username
+    from posts as p
+    join users as u
+        on p.user_id = u.id
+
+
+
     Implement so it resolves this structure:
 
     [
@@ -23,9 +42,29 @@ function findPosts(user_id) {
   */
 }
 
-function find() {
-  return db('users')
+async function find() {
+  const rows = await db('users as u')
+    .leftJoin('posts as p', 'u.id', 'p.user_id')
+    .count('p.id as post_count')
+    .groupBy('u.id')
+    .select('u.id as user_id', 'username')
+    console.log(rows)
+    return rows
+
+
   /*
+    select 
+    u.id as user_id,
+    username,
+    count(p.id) as post_count
+  from users as u
+  left join posts as p 
+    on u.id = p.user_id
+    group by u.id
+
+
+
+  
     Improve so it resolves this structure:
 
     [
@@ -44,9 +83,38 @@ function find() {
   */
 }
 
-function findById(id) {
-  return db('users').where({ id }).first()
+async function findById(id) {
+ const rows = await db('users as u')
+  .leftJoin('posts as p', 'u.id', 'p.user_id')
+  .select('u.id as user_id', 'username', 'contents', 'p.id as post_id')
+  .where('u.id', id)
+  
+
+  let result = rows.reduce((acc, row) => {
+    if (row.contents) {
+      acc.posts.push({ contents: row.contents, post_id: row.post_id })
+    }
+    return  acc
+  }, { user_id: rows[0].user_id, username: rows[0].username, posts: [] })
+
+  return result
+
+
+
   /*
+
+  select 
+    u.id as user_id,
+    username,
+    contents,
+    p.id as post_id
+from users as u
+left join posts as p 
+    on u.id = p.user_id
+where u.id = 1;
+
+
+
     Improve so it resolves this structure:
 
     {
